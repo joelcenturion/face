75f72a38b54f9b9dddf8b9d40de97d05e62f94cc
Joel Centurion <jcenturion@bypersoft.com.py>
2021-11-10 16:03:04 -0300

    10-11-2021

--- Landmark/app/index.php
+++ Landmark/app/index.php
@@ -27,8 +27,27 @@
     $length = count($line2); //cantidad de elementos
     fclose($csv);
   // }
+
+  list($width, $height) = getimagesize('/app/image/image.jpg');
+
+  // echo "width: $width <br/> height: $height";
+  // $white= imagecolorallocate($image, 242, 242, 242);
+  // $white= imagecolorallocate($image, 222, 220, 220);
+  $white= imagecolorallocate($image, 255, 255, 255);
+  for ($x = 15; $x < $width; $x+=15){
+    for($y = 0; $y < $height; $y+=2){
+        imagesetpixel($image, $x, $y, $white);
+    } 
+  }
+  for ($y = 15; $y < $height; $y+=15){
+    for($x = 0; $x < $width; $x+=2){
+        imagesetpixel($image, $x, $y, $white);
+    } 
+  } 
+
   $red = imagecolorallocate($image, 255, 0, 0);
   $blue = imagecolorallocate($image, 0,0,255);
+  $purple = imagecolorallocate($image, 128, 0, 128);
   for($c = 2; $c <= $length/2; $c++){
     imagesetpixel($image, $line2[$c], $line2[$c+68], $blue);
     imagesetpixel($image, $line2[$c]+1, $line2[$c+68], $blue);
@@ -41,6 +60,7 @@
   // for ($i = 2; $i < $length; $i++){
   //     echo "$line2[$i] ";
   //   }
+   
 
   imagepng($image, '/app/image/image2.jpg');
   $base64 = base64_encode(file_get_contents('/app/image/image2.jpg'));
